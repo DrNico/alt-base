070a8cee4455220acd77d2ec63463d1d24463492
Nicolas Godbout <nicolas.godbout@gmail.com>
2015-04-07 14:27:55 -0400

    work on category Set

--- Alt/Category/Set.hs
+++ Alt/Category/Set.hs
@@ -18,10 +18,19 @@ This module provides an implementation of the category __Set__ having small sets
 as objects and functions (defined as relations) as arrows.
 -}
 module Alt.Category.Set (
+		-- * Data types
 		CatSet,
 		ObjSet,
 		EqSet,
-		fromList
+		-- * Special objects
+		initial,
+		terminal,
+		-- * Building __Set__ arrows
+		zero,
+		one,
+		fromList,
+		-- * Queries
+		null
 	) where
 
 -- alt-base modules
@@ -31,9 +40,11 @@ import Alt.Transform.Swap
 
 -- base modules
 import Control.Monad (Functor(..), Monad(..))
+import Data.Bool (Bool(..))
 import Data.Eq (Eq)
 import Data.Int (Int)
 import Data.List (map, zipWith, iterate, (!!), elemIndex)
+import qualified Data.List (elem)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (fst, snd)
 import Data.Typeable (Typeable, Proxy(..), eqT)
@@ -65,8 +76,29 @@ data EqSet a b where
 	EqSet		:: [Int]
 				-> EqSet a b
 
+{- | The initial object of __Set__, the empty set.
+-}
+initial :: (Eq a, Typeable a)
+        => ObjSet a
+initial = ObjSet []
+
+{- | The Zero arrow of __Set__, with source and target its initial object, /i.e./ the
+	empty set.
+-}
+zero :: (Eq a, Eq b, Typeable a, Typeable b)
+     => CatSet a b
+zero = FuncSet [] [] []
+
+{- | The One arrow of __Set__, with source and target its terminal object, /i.e./ 
+	the singleton @{()}@.
+-}
+one :: CatSet () ()
+one = FuncSet [()] [0] [()]
+
 {- | Build an arrow of the category __Set__ from a list of source-target
 tuples.
+
+The objects of the sets themselves are required to be instances of 'Eq'.
 -}
 fromList :: (Eq a, Eq b, Typeable a, Typeable b)
          => [(a,b)] -> CatSet a b
@@ -75,10 +107,44 @@ fromList lst =
 		(map fst lst)
 		(zipWith (\_ i -> i) lst (iterate (+ 1) 0))
 		(map snd lst)
+	-- ## check that it is a genuine function
+
+{- | Returns 'True' if the argument is a zero arrow.
+-}
+null :: CatSet a b -> Bool
+null (FuncSet [] [] _) 	= True
+null _ 					= False
+
+{- | Test if the arrow has the given object as valid target.
+
+Since the domain of the arrow is a singleton, its image is also a singleton.
+The arrow therefore represents a single object, tested to be an element of the
+given set (an object of __Set__).
+-}
+elem :: CatSet (Terminal CatSet) b -> ObjSet b -> Bool
+elem (FuncSet [()] [j] ts) (ObjSet os) =
+	Data.List.elem (ts !! j) os
 
 -----
 -- Instances
 -----
+
+class Category cat => CatTerminal cat where
+	type Terminal cat :: *
+
+	terminal :: Obj cat (Terminal cat)
+
+instance CatTerminal CatSet where
+	type Terminal CatSet = ()
+
+{- | The terminal object of __Set__, a singleton. In this implementation, there
+is a terminal object for each Haskell type. The terminal object is therefore not
+technically unique. However, a pair of such objects are deemed equivalent
+
+> witness terminal terminal :: EqC CatSet a b == Just Proxy
+
+-}
+	terminal = ObjSet [()]
 
 instance Equivalence EqSet where
 	type ObjEq EqSet a = ObjSet a
@@ -92,6 +158,8 @@ instance Equivalence EqSet where
 			perm <- swap $ map (\a -> elemIndex a bs) as
 			return $ EqSet perm
 		-- ## check that this is a bijection
+
+	-- reflect = 
 
 instance Category CatSet where
 	type EqC CatSet a b = EqSet (ObjSet a) (ObjSet b)
